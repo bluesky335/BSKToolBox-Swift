8e2cd8b687eeff11ba8f8fcd1f12c2c5cba1fc0a
刘万林 <liuwanlin@chuangmikeji.com>
2018-04-25 21:17:29 +0800

    Initial Commit

---
+++ BSKToolBox-Swift/BSKToolBox_Swift.h
@@ -0,0 +1,19 @@
+//
+//  BSKToolBox_Swift.h
+//  BSKToolBox-Swift
+//
+//  Created by 刘万林 on 2018/4/25.
+//  Copyright © 2018年 LiuWanLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BSKToolBox_Swift.
+FOUNDATION_EXPORT double BSKToolBox_SwiftVersionNumber;
+
+//! Project version string for BSKToolBox_Swift.
+FOUNDATION_EXPORT const unsigned char BSKToolBox_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSKToolBox_Swift/PublicHeader.h>
+
+
